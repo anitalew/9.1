18fb707553b7af97ae7af680f9919216dce24713
Anita Lew Kiedrowska <anitalk@poczta.onet.pl>
2018-05-21 00:00:12 +0200

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-function getTriangleArea(a, h)
-{
-  if(a <= 0 || h <= 0)
-    {
+function getTriangleArea(a, h) {
+  if(a <= 0 || h <= 0) {
       return "nieprawidłowe dane";
     }
-  var triangleArea = a * h / 2; 
-  return triangleArea;
+  return a * h / 2;
 }
 console.log(getTriangleArea(10, 4));
 
 var triangle1Area = getTriangleArea(9, 3);
-var triangle1Area = getTriangleArea(8, 4);
-var triangle1Area = getTriangleArea(15, 15);
+console.log(triangle1Area);
+
+var triangle2Area = getTriangleArea(8, 4);
+console.log(triangle2Area);
+
+var triangle3Area = getTriangleArea(15, 15);
+console.log(triangle3Area);
